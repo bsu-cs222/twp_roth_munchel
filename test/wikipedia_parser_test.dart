f03d900e6b11cdc8547bb0b81f1f354c918dbf6c
MunchMan34 <cole.munchel@bsu.edu>
2024-09-19 14:21:38 -0400

    Starting code and refactor

--- test/wikipedia_parser_test.dart
+++ test/wikipedia_parser_test.dart
@@ -1,4 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:twp_roth_munchel/wikipedia_parser.dart';
 
+void main() {
+  test('Patch23 made the most recent edit to the Soup page', () async {
+    final jsonEncodedResponse = await File('test/soup.json').readAsString();
+    final jsonObject = jsonDecode(jsonEncodedResponse);
+    final parser = WikipediaParser();
+    final editorName = parser.parse(jsonObject);
+    expect(editorName, 'Patch23');
+  });
+
+  test('GreenC bot made the most recent edit to the Ball State page', () async {
+    final jsonEncodedResponse = await File('test/bsu.json').readAsString();
+    final jsonObject = jsonDecode(jsonEncodedResponse);
+    final parser = WikipediaParser();
+    final editorName = parser.parse(jsonObject);
+    expect(editorName, 'GreenC bot');
+  });
 }
