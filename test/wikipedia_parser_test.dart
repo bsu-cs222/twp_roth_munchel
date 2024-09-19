2abe4e4c4be3a6081d5f53754f54b524c4ecd9cc
BroD-Prod <brodyroth23@gmail.com>
2024-09-19 14:15:39 -0400

    Added WikipediaParser test class

---
+++ test/wikipedia_parser_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:twp_roth_munchel/wikipedia_parser.dart';
+import 'package:twp_roth_munchel/soup.json';
+
+main() {
+  test('Patch23 is the most recent user', () {});
+}
