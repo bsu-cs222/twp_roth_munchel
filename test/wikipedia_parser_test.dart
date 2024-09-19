31a88ad5a8596310c7b37e5fd27e707b1c84d601
BroD-Prod <brodyroth23@gmail.com>
2024-09-19 14:47:21 -0400

    Added searchID function to wikipedia_parser.dart and added tests

--- test/wikipedia_parser_test.dart
+++ test/wikipedia_parser_test.dart
@@ -4,20 +4,20 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:twp_roth_munchel/wikipedia_parser.dart';
 
-void main() {
-  test('Patch23 made the most recent edit to the Soup page', () async {
+main() {
+  final parser = WikipediaParser();
+
+  test('Patch23 is the most recent user', () async {
     final jsonEncodedResponse = await File('test/soup.json').readAsString();
-    final jsonObject = jsonDecode(jsonEncodedResponse);
-    final parser = WikipediaParser();
-    final editorName = parser.parse(jsonObject);
+    final jsonDecoded = jsonDecode(jsonEncodedResponse);
+    final editorName = parser.editorName(jsonDecoded);
     expect(editorName, 'Patch23');
   });
 
-  test('GreenC bot made the most recent edit to the Ball State page', () async {
-    final jsonEncodedResponse = await File('test/bsu.json').readAsString();
-    final jsonObject = jsonDecode(jsonEncodedResponse);
-    final parser = WikipediaParser();
-    final editorName = parser.parse(jsonObject);
-    expect(editorName, 'GreenC bot');
+  test('The PageID is 19651298', () async {
+    final jsonEncodedResponse = await File('test/soup.json').readAsString();
+    final jsonDecoded = jsonDecode(jsonEncodedResponse);
+    final pageId = parser.searchID(jsonDecoded);
+    expect(pageId, 19651298);
   });
 }
