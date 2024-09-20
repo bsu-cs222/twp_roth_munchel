75df9b9395212084990e76133d08476ac0ad7bba
BroD-Prod <brodyroth23@gmail.com>
2024-09-20 11:47:00 -0400

    added http_fetcher.dart and relevant test

--- test/http_test.dart
+++ test/http_test.dart
@@ -1,1 +1,18 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:twp_roth_munchel/http_fetcher.dart';
 import 'package:http/http.dart' as http;
+import 'package:twp_roth_munchel/wikipedia_parser.dart';
+
+main() {
+  test('The soup wikipedia page will be fetched', () async {
+    final fetcher = FetchHttp();
+    final parser = WikipediaParser();
+    final response = fetcher.fetchAlbum();
+    final JsonEncodedResponse =
+        await http.get(Uri.parse('https://en.wikipedia.org/wiki/Soup'));
+    final JsonDecodedResponse = jsonDecode(JsonEncodedResponse) as String;
+    //this doesn't work to decode it
+  });
+}
