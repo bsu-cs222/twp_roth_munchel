59db91e4c405d8f1449cd27f8be1cb1e3b42fd3b
BroD-Prod <brodyroth23@gmail.com>
2024-09-23 13:49:32 -0400

    Tried another new testing method

--- lib/http_fetcher.dart
+++ lib/http_fetcher.dart
@@ -3,9 +3,9 @@ import 'package:http/http.dart' as http;
 import 'album.dart';
 
 class FetchHttp {
-  Future<Album> fetchAlbum(String link) async {
-    link = 'https://en.wikipedia.org/wiki/Soup';
-    final response = await http.get(Uri.parse(link));
+  Future<Album> fetchAlbum() async {
+    final response =
+        await http.get(Uri.parse('https://en.wikipedia.org/wiki/Soup'));
     if (response.statusCode == 200) {
       return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
     } else {
--- test/http_test.dart
+++ test/http_test.dart
@@ -1,19 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:twp_roth_munchel/album.dart';
 import 'package:twp_roth_munchel/http_fetcher.dart';
-import 'package:twp_roth_munchel/wikipedia_parser.dart';
 import 'package:http/http.dart' as http;
 
 main() {
   final fetcher = FetchHttp();
-  final parser = WikipediaParser();
 
   test('The soup wikipedia page will be fetched', () async {
-    final jsonEncodedResponse =
+    final fetchedSoup = fetcher.fetchAlbum();
+    final response =
         await http.get(Uri.parse('https://en.wikipedia.org/wiki/Soup'));
-    final parsedData = parser.parseWikipediaJson(jsonEncodedResponse);
-    final album = Album.fromJson(parsedData);
-    expect(album, parsedData.readAsString);
-    //Try to figure out hpw to map response to Map<String,dynamic>
+    dynamic soupAlbum = Album.fromJson(response.body as Map<String, dynamic>);
+    expect(fetchedSoup, soupAlbum);
+    //The request is going through, but mapping the response to album
+    //is what needs to be done next. Try to figure out how to make our album
+    //exactly like the soup.json file.
+
+    //You can see if you test with coverage
   });
 }
