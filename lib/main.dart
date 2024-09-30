f2f6f130a065b18227300d02f8e62e252e16fc94
BroD-Prod <brodyroth23@gmail.com>
2024-09-30 15:40:52 -0400

    updated ui

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
+    return MaterialApp(
+      title: 'Wikipedia Change Search Page',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
         body: DefaultTextStyle(
-          style: TextStyle(fontSize: 32),
+          style: TextStyle(fontSize: 30),
           child: MyHomePage(),
         ),
       ),
@@ -37,7 +41,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _textController = TextEditingController();
   WikipediaChange changeRecord =
-      WikipediaChange(user: 'Example Guy', timestamp: DateTime.now());
+      WikipediaChange(user: 'User', timestamp: DateTime.now());
 
   final _finalWikipediaUrl = UriBuilder();
   String output = '';
@@ -45,17 +49,35 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          TextField(controller: _textController),
-          ElevatedButton(
-            onPressed: _onButtonPressed,
-            child: const Text('Go'),
+    return Column(
+      children: [
+        Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: TextField(controller: _textController),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: _onButtonPressed,
+                  child: const Text('Go'),
+                ),
+              ),
+              const SizedBox(
+                width: 40,
+                height: 50,
+              ),
+              Column(
+                children: [
+                  Center(child: WikipediaChangeWidget(changeRecord)),
+                ],
+              ),
+              Text(output)
+            ],
           ),
-          WikipediaChangeWidget(changeRecord),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
@@ -63,10 +85,72 @@ class _MyHomePageState extends State<MyHomePage> {
     final theString = await _future;
     setState(() {
       _future = http.read(
-          Uri.parse(_finalWikipediaUrl.finalUrlBuilder(_textController.text)));
+          Uri.parse(_finalWikipediaUrl.finalUrlBuilder(_textController.text)),
+          headers: {
+            'user-agent':
+                'Revision Reporter/0.1 (https://www.cs.bsu.edu/~pvgestwicki/courses/cs222Fa24; brody.roth@bsu.edu/cole.munchel@bsu.edu)'
+          });
       final Map jsonDecoder = jsonDecode(theString!);
       final parser = WikipediaParser();
       output = (parser.parse(jsonDecoder));
     });
+  }
+}
+
+class WikipediaChangeWidget extends StatelessWidget {
+  final WikipediaChange changeRecord;
+
+  const WikipediaChangeWidget(this.changeRecord, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('User: ${changeRecord.user}'),
+        Text('Timestamp: ${changeRecord.timestamp.toString()}'),
+      ],
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                location,
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
